55e2b0109937794c780bea5e32920967f2d6d236
randAyo <aot14121@gmail.com>
2020-08-26 20:07:54 +0100

    upload to degitalocean

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -23,8 +23,34 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
       <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
       <meta name="msapplication-TileColor" content="#da532c" />
       <meta name="theme-color" content="#dcdada"></meta>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-170268181-1"></script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag() { dataLayer.push(arguments); }
+          gtag('js', new Date());
+  
+          gtag('config', 'UA-170268181-1');
+      `,
+        }}
+      />
+      <script dangerouslySetInnerHTML={{
+        __html: `
+        var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+        (function () {
+            var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+            s1.async = true;
+            s1.src = 'https://embed.tawk.to/5f07505b67771f3813c0be7d/default';
+            s1.charset = 'UTF-8';
+            s1.setAttribute('crossorigin', '*');
+            s0.parentNode.insertBefore(s1, s0);
+        })();
+        `
+      }}
+      />
     </Head>
-    <Header  height={400} />
+    <Header height={400} />
     <Socials />
     {children}
     <Footer />
