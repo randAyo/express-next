9604af4d3650859b7a5b445fdb029154d00857fc
randAyo <aot14121@gmail.com>
2020-09-02 00:03:23 +0100

    removed loadable componets

--- pages/services/[id]/index.tsx
+++ pages/services/[id]/index.tsx
@@ -3,12 +3,15 @@ import { NextPage } from 'next';
 import { api, accordion } from '../../../interfaces';
 import { server } from '../../../config'
 import useWindowDimensions from '../../../hoc/useWindow';
+import ServiceWrapper from '../../../components/ServiceWrapper/ServiceWrapper';
+import ServiceContent from '../../../components/ServiceContent/ServiceContent';
+import SideNav from '../../../components/SideNav/SideNav';
 
-import loadable from '@loadable/component';
+// import loadable from '@loadable/component';
 
-const SideNav = loadable(() => import('../../../components/SideNav/SideNav'));
-const ServiceContent = loadable(() => import('../../../components/ServiceContent/ServiceContent'));
-const ServiceWrapper = loadable(() => import('../../../components/ServiceWrapper/ServiceWrapper'));
+// const SideNav = loadable(() => import('../../../components/SideNav/SideNav'));
+// const ServiceContent = loadable(() => import('../../../components/ServiceContent/ServiceContent'));
+// const ServiceWrapper = loadable(() => import('../../../components/ServiceWrapper/ServiceWrapper'));
 
 
 interface PropsPro extends api {
