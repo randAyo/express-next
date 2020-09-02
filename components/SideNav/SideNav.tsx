e27195b206d1800fbe7625f6f9f970373cd7b0eb
randAyo <aot14121@gmail.com>
2020-09-02 17:40:52 +0100

    html

--- components/SideNav/SideNav.tsx
+++ components/SideNav/SideNav.tsx
@@ -1,29 +1,39 @@
 import classes from './SideNav.module.scss'
 import { NameList } from '../../interfaces';
 import Link from 'next/link';
+import { useState } from 'react';
 
 interface productName {
     nameList: NameList[]
 }
 
- const SideNav: React.FC<productName> = ({ nameList }) => {
+const SideNav: React.FC<productName> = ({ nameList }) => {
     const { sidenav, unordered, btn } = classes;
+    let [state, setstate] = useState(null)
+    const showList = () => {
+        const state = [];
+        nameList.map(ex => {
+            state.push(
+                <li key={ex.id} className={btn}>
+                    <Link
+                        href={`/services/${ex.name.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "")}`}>
+                        <p>{ex.name}</p>
+                    </Link>
+                </li>
+            )
+        }
+        )
+        setstate(state);
+    }
+    if (!state && nameList.length) {
+        showList();
+    }
     return (
         <aside className={sidenav}>
             <h1>Produits: </h1>
-            <ul className={unordered}>{nameList.map(ex => {
-                let url = `/services/${ex.name.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "")}`;
-                return (
-                    <li key={ex.id} className={btn}>
-                        <Link
-                            
-                            href={url}>
-                            <p>{ex.name}</p>
-                        </Link>
-                    </li>
-                )
-            }
-            )}</ul>
+            <ul className={unordered}>
+                {state}
+            </ul>
         </aside>
     );
 }
